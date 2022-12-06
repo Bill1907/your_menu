2842d353011357c688885ddf62f125b57dbae4c2
Bill1907 <a31829660@gamil.com>
2022-12-06 22:55:45 +0900

    현재위치 값 가져오는 웹API 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from './layouts/Layout';
+import RadioComponent from './components/input/RadioComponent';
+
+type Location = {
+  latitude: number;
+  longitude: number;
+};
 
 export default function App() {
+  const [location, setLocation] = useState<Location | null>(null);
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      });
+    }
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
   return (
     <Layout title="이거먹어">
-      <div>hh</div>
+      {location ? (
+        <div>
+          <span>{location.latitude}</span>
+          <span>{location.longitude}</span>
+        </div>
+      ) : (
+        <div>
+          <span>값이 없습니당</span>
+        </div>
+      )}
+      <RadioComponent />
     </Layout>
   );
 }
---
+++ src/components/input/RadioComponent.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const RadioInput = styled.input``;
+
+export default function RadioComponent() {
+  return <RadioInput type="radio" />;
+}
